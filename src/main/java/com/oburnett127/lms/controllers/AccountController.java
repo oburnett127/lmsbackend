4c34c13aed05c2b0fa749c7f2a62e1a21b062444
oburnett127 <oburnett127@gmail.com>
2021-11-23 18:03:38 -0600

    Modified db.sql, worked on account classes and purchase.java. Added delete and update account request classes.

--- src/main/java/com/oburnett127/lms/controllers/AccountController.java
+++ src/main/java/com/oburnett127/lms/controllers/AccountController.java
@@ -1,75 +1,75 @@
-package com.oburnett127.lms.controllers;
-
-import com.oburnett127.lms.constants.DebugMessage;
-import com.oburnett127.lms.models.Account;
-import com.oburnett127.lms.models.requests.AccountRequest;
-import com.oburnett127.lms.models.requests.CreateAccountRequest;
-import com.oburnett127.lms.models.requests.DeleteAccountRequest;
-import com.oburnett127.lms.models.requests.UpdateAccountRequest;
-import com.oburnett127.lms.services.AccountOperations;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-import java.io.IOException;
-import java.util.List;
-
-
-@CrossOrigin
-@RestController
-@Slf4j
-public class AccountController {
-
-	private final AccountOperations service;
-
-	public AccountController(final AccountOperations service){
-		this.service = service;
-	}
-
-	@GetMapping("/view")
-	public ResponseEntity<List<Account>> view() {
-		final var result = service.listAll();
-		return ResponseEntity.ok().body(result);
-	}
-
-	@GetMapping("/account")
-	public ResponseEntity<Account> getAccount(@Validated @RequestBody AccountRequest accountRequest) {
-		final Account account = service.getAccount(accountRequest.getId());
-		return ResponseEntity.ok().body(account);
-	}
-
-
-	@PostMapping("/create")
-	public ResponseEntity<Account> createAccount(@Validated @RequestBody CreateAccountRequest createAccountRequest) throws IOException {
-		final var account = Account.builder()
-				.email(createAccountRequest.getEmail())
-				.phone(createAccountRequest.getPhone())
-				.firstName(createAccountRequest.getFirstName())
-				.lastName(createAccountRequest.getLastName())
-				.password(createAccountRequest.getPassword())
-				.isAdmin(createAccountRequest.isAdmin())
-				.isAuthor(createAccountRequest.isAuthor())
-				.build();
-		service.createAccount(account);
-		log.debug(DebugMessage.MSG5,account.getFirstName() + " " + account.getLastName(),account.getId());
-		return ResponseEntity.ok(account);
-	}
-
-	@PostMapping("/update")
-	public ResponseEntity<Account> updateAccount(@Validated @RequestBody UpdateAccountRequest updateAccountRequest) throws IOException {
-		final var id = updateAccountRequest.getId();
-		final var amount = updateAccountRequest.getAmount();
-		final var result = service.updateAccount(id, amount);
-		log.debug(DebugMessage.MSG6, amount, result.getId(), result.getBalance());
-		return ResponseEntity.ok().body(result);
-	}
-
-	@PostMapping("/delete")
-	public ResponseEntity<Account> deleteAccount(@Validated @RequestBody DeleteAccountRequest deleteAccountRequest) throws IOException {
-		final var id = deleteAccountRequest.getId();
-		final var amount = deleteAccountRequest.getAmount();
-		final var result = service.deleteAccount(id, amount);
-		log.debug(DebugMessage.MSG6, amount, result.getId(), result.getBalance());
-		return ResponseEntity.ok().body(result);
-	}
-}
+//package com.oburnett127.lms.controllers;
+//
+//import com.oburnett127.lms.constants.DebugMessage;
+//import com.oburnett127.lms.models.Account;
+//import com.oburnett127.lms.models.requests.AccountRequest;
+//import com.oburnett127.lms.models.requests.CreateAccountRequest;
+//import com.oburnett127.lms.models.requests.DeleteAccountRequest;
+//import com.oburnett127.lms.models.requests.UpdateAccountRequest;
+//import com.oburnett127.lms.services.AccountOperations;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.*;
+//import java.io.IOException;
+//import java.util.List;
+//
+//
+//@CrossOrigin
+//@RestController
+//@Slf4j
+//public class AccountController {
+//
+//	private final AccountOperations service;
+//
+//	public AccountController(final AccountOperations service){
+//		this.service = service;
+//	}
+//
+//	@GetMapping("/view")
+//	public ResponseEntity<List<Account>> view() {
+//		final var result = service.listAll();
+//		return ResponseEntity.ok().body(result);
+//	}
+//
+//	@GetMapping("/account")
+//	public ResponseEntity<Account> getAccount(@Validated @RequestBody AccountRequest accountRequest) {
+//		final Account account = service.getAccount(accountRequest.getId());
+//		return ResponseEntity.ok().body(account);
+//	}
+//
+//
+//	@PostMapping("/create")
+//	public ResponseEntity<Account> createAccount(@Validated @RequestBody CreateAccountRequest createAccountRequest) throws IOException {
+//		final var account = Account.builder()
+//				.email(createAccountRequest.getEmail())
+//				.phone(createAccountRequest.getPhone())
+//				.firstName(createAccountRequest.getFirstName())
+//				.lastName(createAccountRequest.getLastName())
+//				.password(createAccountRequest.getPassword())
+//				.isAdmin(createAccountRequest.isAdmin())
+//				.isAuthor(createAccountRequest.isAuthor())
+//				.build();
+//		service.createAccount(account);
+//		log.debug(DebugMessage.MSG5,account.getFirstName() + " " + account.getLastName(),account.getId());
+//		return ResponseEntity.ok(account);
+//	}
+//
+//	@PostMapping("/update")
+//	public ResponseEntity<Account> updateAccount(@Validated @RequestBody UpdateAccountRequest updateAccountRequest) throws IOException {
+//		final var id = updateAccountRequest.getId();
+//		final var amount = updateAccountRequest.getAmount();
+//		final var result = service.updateAccount(id, amount);
+//		log.debug(DebugMessage.MSG6, amount, result.getId(), result.getBalance());
+//		return ResponseEntity.ok().body(result);
+//	}
+//
+//	@PostMapping("/delete")
+//	public ResponseEntity<Account> deleteAccount(@Validated @RequestBody DeleteAccountRequest deleteAccountRequest) throws IOException {
+//		final var id = deleteAccountRequest.getId();
+//		final var amount = deleteAccountRequest.getAmount();
+//		final var result = service.deleteAccount(id, amount);
+//		log.debug(DebugMessage.MSG6, amount, result.getId(), result.getBalance());
+//		return ResponseEntity.ok().body(result);
+//	}
+//}
--- src/main/java/com/oburnett127/lms/services/AccountService.java
+++ src/main/java/com/oburnett127/lms/services/AccountService.java
@@ -1,115 +1,115 @@
-package com.oburnett127.lms.services;
-
-import com.oburnett127.lms.daos.AccountDao;
-import com.oburnett127.lms.models.Account;
-import com.oburnett127.lms.utils.AccountValidator;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.UUID;
-
-@Service
-@Slf4j
-public class AccountService implements AccountOperations {
-
-	private final AccountDao accountDao;
-	private final AccountValidator accountValidator;
-
-	public AccountService(final AccountDao accountDao, final AccountValidator accountValidator) {
-		this.accountDao = accountDao;
-		this.accountValidator = accountValidator;
-	}
-
-	@Override
-	public List<Account> listAll() {
-		return this.accountDao.getAll();
-	}
-
-	@Override
-	public void createAccount(Account account) {
-		this.accountDao.create(account);
-	}
-
-	@Override
-	@SneakyThrows
-	public Account getAccount(final UUID id) {
-		final var account = accountDao.getAccount(id);
-
-		return account;
-	}
-
-	@Override
-	public Account updateAccount(Account Account) {
-		return null;
-	}
-
-	@Override
-	public void deleteAccount(Integer id) {
-
-	}
-
-	@Override
-	@SneakyThrows
-	public Account withdraw(UUID id, BigDecimal amount) {
-		final var account = accountDao.getAccount(id);
-
-		accountValidator.withdraw(account, amount);
-
-		account.setBalance(account.getBalance().subtract(amount));
-
-		accountDao.save(account);
-
-		return account;
-	}
-
-	@Override
-	@SneakyThrows
-	public Account deposit(UUID id, BigDecimal amount) {
-		final var account = accountDao.getAccount(id);
-
-		accountValidator.deposit(id, amount);
-
-		log.debug("account.getId() {}", account.getId());
-		log.debug("account balance: {} amount: {}", account.getBalance(), amount);
-
-		account.setBalance(account.getBalance().add(amount));
-
-		accountDao.save(account);
-
-		return account;
-	}
-
-	@Override
-	@SneakyThrows
-	public Account depositCheck(UUID id, String fullName, String signature, BigDecimal amount) {
-		final var account = accountDao.getAccount(id);
-
-		accountValidator.depositCheck(id, fullName, signature, amount);
-
-		account.setBalance(account.getBalance().add(amount));
-
-		accountDao.save(account);
-
-		return account;
-	}
-
-	@Override
-	@SneakyThrows
-	public Account transfer(UUID idSender, UUID idReceiver, BigDecimal amount) {
-		final var senderAccount = accountDao.getAccount(idSender);
-		final var receiverAccount = accountDao.getAccount(idReceiver);
-
-		accountValidator.transfer(senderAccount, receiverAccount, amount);
-
-		senderAccount.setBalance(senderAccount.getBalance().subtract(amount));
-		receiverAccount.setBalance(receiverAccount.getBalance().add(amount));
-
-		accountDao.save(senderAccount);
-		accountDao.save(receiverAccount);
-
-		return senderAccount;
-	}
-}
+//package com.oburnett127.lms.services;
+//
+//import com.oburnett127.lms.daos.AccountDao;
+//import com.oburnett127.lms.models.Account;
+//import com.oburnett127.lms.utils.AccountValidator;
+//import lombok.SneakyThrows;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//
+//import java.math.BigDecimal;
+//import java.util.List;
+//import java.util.UUID;
+//
+//@Service
+//@Slf4j
+//public class AccountService implements AccountOperations {
+//
+//	private final AccountDao accountDao;
+//	private final AccountValidator accountValidator;
+//
+//	public AccountService(final AccountDao accountDao, final AccountValidator accountValidator) {
+//		this.accountDao = accountDao;
+//		this.accountValidator = accountValidator;
+//	}
+//
+//	@Override
+//	public List<Account> listAll() {
+//		return this.accountDao.getAll();
+//	}
+//
+//	@Override
+//	public void createAccount(Account account) {
+//		this.accountDao.create(account);
+//	}
+//
+//	@Override
+//	@SneakyThrows
+//	public Account getAccount(final UUID id) {
+//		final var account = accountDao.getAccount(id);
+//
+//		return account;
+//	}
+//
+//	@Override
+//	public Account updateAccount(Account Account) {
+//		return null;
+//	}
+//
+//	@Override
+//	public void deleteAccount(Integer id) {
+//
+//	}
+//
+//	@Override
+//	@SneakyThrows
+//	public Account withdraw(UUID id, BigDecimal amount) {
+//		final var account = accountDao.getAccount(id);
+//
+//		accountValidator.withdraw(account, amount);
+//
+//		account.setBalance(account.getBalance().subtract(amount));
+//
+//		accountDao.save(account);
+//
+//		return account;
+//	}
+//
+//	@Override
+//	@SneakyThrows
+//	public Account deposit(UUID id, BigDecimal amount) {
+//		final var account = accountDao.getAccount(id);
+//
+//		accountValidator.deposit(id, amount);
+//
+//		log.debug("account.getId() {}", account.getId());
+//		log.debug("account balance: {} amount: {}", account.getBalance(), amount);
+//
+//		account.setBalance(account.getBalance().add(amount));
+//
+//		accountDao.save(account);
+//
+//		return account;
+//	}
+//
+//	@Override
+//	@SneakyThrows
+//	public Account depositCheck(UUID id, String fullName, String signature, BigDecimal amount) {
+//		final var account = accountDao.getAccount(id);
+//
+//		accountValidator.depositCheck(id, fullName, signature, amount);
+//
+//		account.setBalance(account.getBalance().add(amount));
+//
+//		accountDao.save(account);
+//
+//		return account;
+//	}
+//
+//	@Override
+//	@SneakyThrows
+//	public Account transfer(UUID idSender, UUID idReceiver, BigDecimal amount) {
+//		final var senderAccount = accountDao.getAccount(idSender);
+//		final var receiverAccount = accountDao.getAccount(idReceiver);
+//
+//		accountValidator.transfer(senderAccount, receiverAccount, amount);
+//
+//		senderAccount.setBalance(senderAccount.getBalance().subtract(amount));
+//		receiverAccount.setBalance(receiverAccount.getBalance().add(amount));
+//
+//		accountDao.save(senderAccount);
+//		accountDao.save(receiverAccount);
+//
+//		return senderAccount;
+//	}
+//}
